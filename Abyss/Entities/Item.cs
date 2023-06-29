91b169a080952b064a6cad8909aeda8912de6834
kylecarter380@gmail.com <kylecarter380@gmail.com>
2023-06-28 20:40:12 -0700

    added inventory infrastructure (both coded & in save file). Unable to read inventory from save nor can it use items, items are currently blank.

---
+++ Abyss/Entities/Item.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abyss.Entities
+{
+    internal abstract class Item
+    {
+    }
+
+    internal sealed class Grimoire : Item
+    {
+        public static Grimoire Base;
+
+        public static Grimoire Water;
+        public static Grimoire Wind;
+        public static Grimoire Earth;
+        public static Grimoire Fire;
+        public static Grimoire Lightning;
+    }
+
+    internal sealed class Card : Item
+    {
+        /// <summary>
+        /// Enables the player to place a temporary respawn point (time dilation) that is 1 time use
+        /// </summary>
+        public static Card Time;
+
+        /// <summary>
+        /// Increases the player's max speed by 50% of their current health for 30 seconds
+        /// </summary>
+        public static Card Wind;
+
+        /// <summary>
+        /// Increases the player's defense by 50% of their current resistence for 30 seconds
+        /// </summary>
+        public static Card Earth;
+
+        /// <summary>
+        /// Sets the player on fire, while on fire the player's damage is increased by 25% of their max HP - current HP for 30s
+        /// </summary>
+        public static Card Fire;
+
+        /// <summary>
+        /// TBD
+        /// </summary>
+        public static Card Lightning;
+
+        /// <summary>
+        /// Makes the player wet, and increases their crit rate by 70% of their current crit damage.
+        /// </summary>
+        public static Card Water;
+    }
+}
--- Abyss/Entities/Player.cs
+++ Abyss/Entities/Player.cs
@@ -13,21 +13,50 @@ using System.Threading.Tasks;
 
 namespace Abyss.Entities
 {
+    internal struct Inventory
+    {
+        public Grimoire[] grimoires; // max = 2, can only hold 2 grimoires
+        public Card[] cards; // max total = 8
+        public int[] regenPots; // array max = 2, combined value max = 16  (both are counters), [0] = health, [1] = mana
+        public int[] instantPots; // array max = 2, combiend value max = 4 (see line above)
+        public List<Item> extras; // unlimited number of extra items, these are mainly for questlines or auxillary stuff so not entirely important
+    }
+
     internal class Player : Entity
     {
+        // declare the player's inventory
+        public Inventory Inventory;
+
         public Player(Texture2D texture) :base(texture)
         {
-            // BOTH OF THESE ARE PLACEHOLDERS
+            // draw object is currently a placeholder as there is no player texture as of now
             this.drawObj = new Rectangle(0, 0, 16, 16);
-            this.pos = new Vector2(5 * 16, 5 * 16);
+            this.pos = new Vector2();
             this.speed = 2;
         }
 
-        /**
-         * Determines the input vector of the player and normalizes it to a radius of 1
-         * 
-         * @param   KeyboardState   the current keyboard state of the game (what keys are being pressed)
-         */
+        /// <summary>
+        /// loads the given save data into the player state
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <param name="hp"></param>
+        /// <param name="maxHP"></param>
+        /// <param name="mana"></param>
+        /// <param name="maxMana"></param>
+        public void LoadSave(Vector2 pos, int hp, int maxHP, int mana, int maxMana)
+        {
+            this.pos = pos;
+            this.health = hp;
+            this.max_health = maxHP;
+            this.mana = mana;
+            this.max_mana = maxMana;
+        }
+
+
+        /// <summary>
+        /// Determines the input vector of the player and normalizes it to a radius of 1
+        /// </summary>
+        /// <param name="KB"></param>
         public void CalcInputVector(KeyboardState KB)
         {
             // If a key is being pressed it is = 1, if it is not then it is = 0
@@ -39,33 +68,32 @@ namespace Abyss.Entities
                 this.movement_vec.Normalize();
         }
 
-        public void LoadSave(Vector2 pos, int hp, int maxHP, int mana, int maxMana)
-        {
-            this.pos = pos;
-            this.health = hp;
-            this.max_health = maxHP;
-            this.mana = mana;
-            this.max_mana = maxMana;
-        }
 
+        /// <summary>
+        /// Returns which side the player is leaving on the current level's map
+        /// </summary>
+        /// <returns>which side the player is leaving, returns nullable if they aren't leaving</returns>
         public Side? ExittingSide()
         {
+            // Check on the x-axis, LEFT / RIGHT
             switch (pos.X)
             {
-                case -1:
-                    return Side.LEFT;
-                case 16 * 16 - 16 + 1:
-                    return Side.RIGHT;
+                case -1: // if they are just left of the game window
+                    return Side.LEFT; // left
+                case 16 * 16 - 16 + 1: // if they are just right of the game window
+                    return Side.RIGHT; // right
                 default: break;
             }
+            // Check on the y-axis, TOP / BOTTOM
             switch (pos.Y)
             {
-                case -1:
-                    return Side.TOP;
-                case 16 * 16 - 16 + 1:
-                    return Side.BOTTOM;
+                case -1: // if they are just above the game window (map)
+                    return Side.TOP; // then its the top
+                case 16 * 16 - 16 + 1: // if they are just below it
+                    return Side.BOTTOM; // then its the bottom
                 default: break;
             }
+            // if not a single check passed then they aren't leaving the map
             return null;
         }
     }
