0cddc3da95e6c961c1d244d3495a06c5bf7c2bd1
Kyle Carter <jackhayes241@gmail.com>
2023-08-21 22:16:27 -0700

    currently broken, half finished dungeon generation code

--- Abyss/Levels/data/Dungeon.cs
+++ Abyss/Levels/data/Dungeon.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Abyss.Levels.dungeons
+namespace Abyss.Levels.data
 {
-    internal struct DungeonData
+    internal class Dungeon
+
     {
         /// <summary>
         /// the maximum width that the dungeon can be, (dungeon spawn is in the center, width/2)
@@ -31,6 +34,41 @@ namespace Abyss.Levels.dungeons
         internal double divergence_rate;
 
 
+
+        /// <summary>
+        /// the array of nodes within the dungeon
+        /// </summary>
+        internal MapNode[] central;
+
+        /// <summary>
+        /// the paths for the system
+        /// </summary>
+        internal MapPath[] paths;
+
+
+
+        /// <summary>
+        /// initializes a new dungeon
+        /// </summary>
+        public Dungeon(int end_y, int width, int max_path, double divergence_rate)
+        {
+            this.end_y = end_y;
+            this.width = width;
+            this.max_path = max_path;
+            this.divergence_rate = divergence_rate;
+
+            this.nodes = Generate();
+        }
+
+
+
+
+        private MapNode[] Generate()
+        {
+
+        }
+
+
         /// <summary>
         /// calculates the median line which seperates the dungeon between its path's divergence or convergence
         /// </summary>
@@ -45,7 +83,7 @@ namespace Abyss.Levels.dungeons
         /// </summary>
         /// <param name="y"></param>
         /// <returns></returns>
-        public double CalculateDiveregenceRate(int y)
+        internal double CalculateDiveregenceRate(int y)
         {
             if (y >= end_y || y < 0) return -1;
             return Math.Cbrt((y - GetMedian()) / (-GetMedian() / Math.Pow(divergence_rate, 3)));
---
+++ Abyss/Levels/data/MapNode.cs
@@ -0,0 +1,113 @@
+﻿using Abyss.Utility;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abyss.Levels.data
+{
+    internal class MapNode
+    {
+        /// <summary>
+        /// the position of the node
+        /// </summary>
+        internal Vector position;
+
+
+        /// <summary>
+        /// whether or not the node is converged to the center
+        /// </summary>
+        internal bool converged;
+
+
+        /// <summary>
+        /// the type of room it is;
+        /// 0 = generic; 1 = entrance; 2 = midpoint; 3 = end; etc.
+        /// </summary>
+        internal byte type;
+
+
+        /// <summary>
+        /// the adjacent nodes that they direct to; index of -1 means blocked off; index of -2 means open but disconnected
+        /// </summary>
+        // bottom = 0; left = 1; right = 2; top = 3;
+        internal int[] next_nodes = new int[4];
+
+
+        /// <summary>
+        /// create a new map node
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="path_id"></param>
+        /// <param name="nodes"></param>
+        public MapNode(int x, int y, byte path_id, byte type, int path, Dungeon dungeon, MapNode previous = null)
+        {
+            this.type = type;
+            position = new Vector(x, y);
+
+            if (previous == null)
+                next_nodes = new int[4] { -1, -2, -2, -2 };
+            else
+                next_nodes = Connect(previous, dungeon, path);
+        }
+
+
+
+        /// <summary>
+        /// connects the map node to the previous and its surroundings
+        /// </summary>
+        /// <returns></returns>
+        private int[] Connect(MapNode builder, Dungeon dungeon, int path)
+        {
+            // completely open
+            next_nodes = new int[4] { -2, -2, -2, -2 };
+
+            // if there is a path to the left or right of the node; BLOCK IT
+            if (!converged)
+            {
+                MapNode[] adjacent_paths = dungeon.AdjacentPath(this, path);
+
+                // check the left side
+                if (adjacent_paths[0] is not null) next_nodes[1] = -1;
+
+                // check the right side
+                if (adjacent_paths[1] is not null) next_nodes[2] = -1;
+            }
+
+            // if the previous was next it;             BLOCK THE BOTTOM
+            if (builder.position.y == this.position.y)
+            {
+                next_nodes[0] = -1;
+
+                if (builder.position.x > this.position.x) next_nodes[2] = dungeon.IndexOf(builder);
+            }
+
+        }
+
+
+
+        /// <summary>
+        /// returns which connection type the node is
+        /// </summary>
+        /// <returns></returns>
+        // 0 = all; 1 = bottom-left; 2 = bottom-right; 3 = bottom; 4 = end; 5 = left-right; 6 = midpoint; 7 = top-bottom; 8 = top-left; 9 = top-right; 10 = floating;
+        public int GetConnectionType()
+        {
+            switch (type)
+            {
+                case 0: // <---- placeholder
+                    {
+                        return 1;
+                    }
+                case 1: return 0;
+                case 2: return 6;
+                case 3: return 4;
+            }
+            return 10;
+        }
+    }
+}
